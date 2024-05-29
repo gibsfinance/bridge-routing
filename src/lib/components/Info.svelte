90a3944fab500ec222d6369e47aaa7c1946e9e06
gibsfinancedev <162373480+gibsfinancedev@users.noreply.github.com>
2024-05-29 10:23:20 -0500

    Adds links to info page to assist with escape hatch

--- src/lib/components/Info.svelte
+++ src/lib/components/Info.svelte
@@ -1,9 +1,22 @@
 <script lang="ts">
   import Icon from '@iconify/svelte'
+  import { onMount } from 'svelte'
   const openModal = () => {
     const el = document.querySelector('#bridge-info-modal') as any
     el.showModal()
   }
+  const bridgeUrl = 'https://bridge.pulsechain.com/'
+  let directUrl = ''
+  const getDirectUrl = () => {
+    fetch(`${bridgeUrl}version.json`)
+      .then((res) => res.json())
+      .then((j) => {
+        directUrl = j.ipfs_gateways[0]
+      })
+  }
+  onMount(() => {
+    getDirectUrl()
+  })
 </script>
 
 <div class="flex flex-row mb-8">
@@ -26,18 +39,23 @@
       <ul class="gap-4 flex flex-col">
         <li class="flex">
           This UI uses the bridge contracts directly, so all of the same security assumptions are
-          present
+          present.
         </li>
         <li>
-          The only difference is that the final router contract on Ethereum has been swapped out
+          The only difference is that the final router contract on Ethereum has been swapped out.
         </li>
         <li>
           This modified implementation allows the runner's actions to be measured and creates an
           economic incentive to deliver native tokens, maintaining privacy for the end user (you).
         </li>
         <li>
-          If, for any reason, your transaction is not run in a timely manner, you can always use the
-          escape hatch to complete the bridge yourself and pay zero fees
+          If, for any reason, your transaction is not run in a timely manner, you can always use <span
+            class="inline"
+            ><a class="underline" target="_blank" href="https://bridge.pulsechain.com"
+              >bridge.pulsechain.com</a>
+            <a href={directUrl} target="_blank"
+              ><Icon icon="gis:direct" class="inline ml-2 mr-1" height="2em" width="2em" /></a>
+          </span> to complete the bridge yourself and pay zero fees outside of the bridge fees.
         </li>
       </ul>
     </div>
