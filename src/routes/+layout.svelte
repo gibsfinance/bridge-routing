55d87cd0b2e365393be837432f12d13c808784c4
gibsfinancedev <162373480+gibsfinancedev@users.noreply.github.com>
2024-05-30 11:06:41 -0500

    Adds title to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,20 @@
   import Nav from '$lib/components/Nav.svelte'
 
   import AuthProvider from '$lib/stores/auth/AuthProvider.svelte'
+  import payMe from '$lib/images/pay-me.png'
+  import bridge from '$lib/images/bridge.jpeg'
+  import futureBridge from '$lib/images/future-bridge.webp'
+  import fingers from '$lib/images/1FAF0.svg'
+  const preloadImageUrls = [payMe, bridge, futureBridge, fingers]
 </script>
+
+<svelte:head>
+  {#each preloadImageUrls as image}
+    <link rel="preload" as="image" href={image} />
+  {/each}
+  <meta name="robots" content="noindex nofollow" />
+  <html lang="en" />
+</svelte:head>
 
 <AuthProvider>
   <div class="app bg-slate-50">
