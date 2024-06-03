ec6542cc3337d8c0210d73470865cef4995b1299
gibsfinancedev <162373480+gibsfinancedev@users.noreply.github.com>
2024-06-02 22:34:42 -0500

    Adds bridge footer component

---
+++ src/lib/components/BridgeFooter.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import Info from './Info.svelte'
+  import Icon from '@iconify/svelte'
+  import Version from './Version.svelte'
+</script>
+
+<div class="flex flex-row mb-8 leading-8 text-slate-400 md:px-0 px-4">
+  <span class="flex hover:text-slate-500">
+    <Info />
+  </span>
+  <span class="flex flex-grow justify-center">
+    <span class="hover:text-slate-500 items-center flex flex-row text-sm" style="line-height: 32px">
+      <Icon icon="mdi:clock-outline" height="1.5em" width="1.5em" />
+      <span class="mx-1">~30 min</span>
+    </span>
+  </span>
+  <span class="flex hover:text-slate-500 text-sm" style="line-height: 32px">
+    <Version />
+  </span>
+</div>
--- src/routes/bridge/[route]/+page.svelte
+++ src/routes/bridge/[route]/+page.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
   import BluryImage from '$lib/components/BluryImage.svelte'
   import Bridge from '$lib/components/Bridge.svelte'
-  import Info from '$lib/components/Info.svelte'
   import bridgeImage from '$lib/images/bridge.jpeg'
   import TokenDirectionDropdown from '$lib/components/TokenDirectionDropdown.svelte'
-  import Version from '$lib/components/Version.svelte'
-  import Icon from '@iconify/svelte'
+  import BridgeFooter from '$lib/components/BridgeFooter.svelte'
 </script>
 
 <BluryImage height="300px" image_url={bridgeImage}>
@@ -18,18 +16,5 @@
 <div class="container m-auto max-w-lg flex flex-col">
   <TokenDirectionDropdown />
   <Bridge />
-  <div class="flex flex-row mb-8 leading-8 text-slate-400 md:px-0 px-4">
-    <span class="flex hover:text-slate-500">
-      <Info />
-    </span>
-    <span class="flex flex-grow justify-center">
-      <span class="hover:text-slate-500 items-center flex flex-row text-sm" style="line-height: 32px">
-        <Icon icon="mdi:clock-outline" height="1.5em" width="1.5em" />
-        <span class="mx-1">~30 min</span>
-      </span>
-    </span>
-    <span class="flex hover:text-slate-500 text-sm" style="line-height: 32px">
-      <Version />
-    </span>
-  </div>
+  <BridgeFooter />
 </div>
