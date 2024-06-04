e761ff5444c511bb2d6d2cc8eedc7d34fd550b2e
gibsfinancedev <162373480+gibsfinancedev@users.noreply.github.com>
2024-06-04 15:05:58 -0500

    Makes delivery headline smaller for small height screens

--- src/routes/delivery/[route]/+page.svelte
+++ src/routes/delivery/[route]/+page.svelte
@@ -5,11 +5,14 @@
   import TokenDirectionDropdown from '$lib/components/TokenDirectionDropdown.svelte'
   import BridgeFooter from '$lib/components/BridgeFooter.svelte'
   import { windowStore } from '$lib/stores/window'
+  $: large = $windowStore.innerHeight > 600 && $windowStore.innerWidth >= 768
 </script>
 
-<BlurryImage height="{$windowStore.innerWidth >= 768 ? 300 : 100}px" image_url={bridgeImage}>
+<BlurryImage height="{large ? 300 : 100}px" image_url={bridgeImage}>
   <h1
-    class="text-white font-italiana z-10 absolute top-0 bottom-0 left-0 right-0 m-auto text-center text-5xl md:text-8xl content-center capitalize">
+    class="text-white font-italiana z-10 absolute top-0 bottom-0 left-0 right-0 m-auto text-center content-center capitalize"
+    class:text-5xl={!large}
+    class:md:text-8xl={large}>
     token delivery
   </h1>
 </BlurryImage>
