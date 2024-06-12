a30b5ef78e9cb20c2a0d62134aae3a3f1c45578a
gibsfinancedev <162373480+gibsfinancedev@users.noreply.github.com>
2024-06-12 00:06:20 -0500

    Adds minimal remapping of images

--- src/lib/components/AssetWithNetwork.svelte
+++ src/lib/components/AssetWithNetwork.svelte
@@ -9,14 +9,26 @@
 
   $: chain = chainsMetadata[asset.hostedNetwork]
   $: remap = new Map<string, string>([
-    ['0x02DcdD04e3F455D838cd1249292C58f3B79e3C3C', `1/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2`],
+    ['0x02DcdD04e3F455D838cd1249292C58f3B79e3C3C', '1/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'],
+    // [
+    //   '0x02DcdD04e3F455D838cd1249292C58f3B79e3C3C',
+    //   `7fc959085dc2b96734a6e51c82086cb6b65aa5fe4492fe974f4b0ab7ba02d480/1/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2.raster`,
+    // ],
+    // [
+    //   '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+    //   `7fc959085dc2b96734a6e51c82086cb6b65aa5fe4492fe974f4b0ab7ba02d480/1/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2.raster`,
+    // ],
   ])
 
-  $: remapped = remap.get(asset.address) || `${asset.address}`
+  $: remapped = remap.get(asset.address) || `${chain.id}/${asset.address}`
+  $: console.log(asset.address, remapped)
 </script>
 
 <span class="token-image-container relative" style="--token-size: {tokenSize};">
-  <img src="{imageRoot}/image/{chain.id}/{remapped}" alt="" class="rounded-full overflow-hidden shadow-md" />
+  <img
+    src="{imageRoot}/image/7fc959085dc2b96734a6e51c82086cb6b65aa5fe4492fe974f4b0ab7ba02d480/{remapped}"
+    alt=""
+    class="rounded-full overflow-hidden shadow-md" />
   <img
     class="network-image absolute -bottom-1 -right-1 bg-slate-100 rounded-full"
     style="--network-size: {networkSize};"
