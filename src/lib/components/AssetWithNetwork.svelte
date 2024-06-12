d4c74c5974329864466947d697db68a3b7ce5603
gibsfinancedev <162373480+gibsfinancedev@users.noreply.github.com>
2024-06-12 00:30:32 -0500

    Adds remapping

--- src/lib/components/AssetWithNetwork.svelte
+++ src/lib/components/AssetWithNetwork.svelte
@@ -10,6 +10,7 @@
   $: chain = chainsMetadata[asset.hostedNetwork]
   $: remap = new Map<string, string>([
     ['0x02DcdD04e3F455D838cd1249292C58f3B79e3C3C', '1/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'],
+    ['0x518076CCE3729eF1a3877EA3647a26e278e764FE', '56/0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c'],
     // [
     //   '0x02DcdD04e3F455D838cd1249292C58f3B79e3C3C',
     //   `7fc959085dc2b96734a6e51c82086cb6b65aa5fe4492fe974f4b0ab7ba02d480/1/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2.raster`,
@@ -26,7 +27,7 @@
 
 <span class="token-image-container relative" style="--token-size: {tokenSize};">
   <img
-    src="{imageRoot}/image/7fc959085dc2b96734a6e51c82086cb6b65aa5fe4492fe974f4b0ab7ba02d480/{remapped}"
+    src="{imageRoot}/image/fallback/7fc959085dc2b96734a6e51c82086cb6b65aa5fe4492fe974f4b0ab7ba02d480/{remapped}"
     alt=""
     class="rounded-full overflow-hidden shadow-md" />
   <img
