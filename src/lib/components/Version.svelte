a27e89de59d148a8358c49b023956321f4904b95
gibsfinancedev <162373480+gibsfinancedev@users.noreply.github.com>
2024-07-18 12:23:12 -0500

    Adds timestamp to version

--- src/lib/components/Version.svelte
+++ src/lib/components/Version.svelte
@@ -5,6 +5,8 @@
   const isLocal = $page.url.host.includes('localhost')
 </script>
 
-<span class="flex text-center font-mono font-thin">
+<span
+  class="flex text-center font-mono font-thin tooltip tooltip-left"
+  data-tip={new Date(+env.version || 0).toISOString()}>
   v{appVersion}@{isLocal ? 'local' : env.version}
 </span>
