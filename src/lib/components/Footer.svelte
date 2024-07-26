ffee537ec0e41322e965c1379ac5441c545cff59
gibsfinancedev <162373480+gibsfinancedev@users.noreply.github.com>
2024-07-26 13:48:46 -0500

    Adds optimization

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -6,12 +6,12 @@
 <div class="flex flex-row leading-8 text-slate-200 bg-slate-950 p-4 justify-around">
   <div class="max-w-5xl flex flex-row flex-grow container">
     <div class="flex flex-grow justify-between">
-      <span class="flex">
+      <div class="flex">
         <Info />
-      </span>
+      </div>
     </div>
-    <span class="flex hover:text-slate-400 text-xs items-center sm:text-sm leading-8">
+    <div class="flex hover:text-slate-400 text-xs items-center sm:text-sm leading-8">
       <Version />
-    </span>
+    </div>
   </div>
 </div>
