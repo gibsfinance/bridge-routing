f6d7184080bf634d897e17bd7b9f516fba5ab021
gibsfinancedev <162373480+gibsfinancedev@users.noreply.github.com>
2024-11-05 17:23:03 -0600

    Add Toast component and store for message handling in Svelte
    - Implement Toast.svelte for displaying toast notifications
    - Create toast store with addMessage and removeMessage functions
    - Define Message type for structured message data

---
+++ src/lib/stores/toast.ts
@@ -0,0 +1,46 @@
+import { writable } from 'svelte/store'
+import { chainsMetadata } from '$lib/stores/auth/constants'
+import { Chains } from '$lib/stores/auth/types'
+
+export type Message = {
+  message: string
+  link: string
+  label: string
+  timeout?: number
+}
+
+export const messages = writable<Message[]>([])
+
+export const addMessage = (msg: Message) => {
+  console.log('addMessage', msg)
+  messages.update((msgs) => [...msgs, msg])
+  setTimeout(() => {
+    messages.update((msgs) => {
+      const index = msgs.findIndex((m) => m === msg)
+      return msgs.slice(0).splice(index, 1)
+    })
+  }, msg.timeout || 20_000)
+}
+
+// const uri = (chainId: Chains, type: 'tx' | 'address' = 'tx', suffix: string) => {
+//   const url = chainsMetadata[Chains.PLS].blockExplorers?.default.url
+//   return `${url}/${chainId === Chains.V4PLS ? '#/' : ''}${type}/${suffix}`
+// }
+
+// const loop = () => {
+//   addMessage({
+//     message: 'Hello',
+//     link: 'https://google.com',
+//     label: 'Hello',
+//     timeout: 10_000 + 10_000 * Math.random(),
+//   })
+//   setTimeout(() => {
+//     loop()
+//   }, 10_000 + 10_000 * Math.random())
+// }
+
+// loop()
+
+export const removeMessage = (msg: Message) => {
+  messages.update((msgs) => msgs.filter((m) => m !== msg))
+}
