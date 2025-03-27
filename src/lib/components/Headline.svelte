f905f873ce160571554e543cc5cc53a41b6e9652
gibsfinancedev <162373480+gibsfinancedev@users.noreply.github.com>
2025-03-27 16:14:42 -0500

    style(Headline): add font-bold class to headline component

--- src/lib/components/Headline.svelte
+++ src/lib/components/Headline.svelte
@@ -9,7 +9,7 @@
   const { children, class: className = '' }: Props = $props()
   const classes = $derived(
     classNames(
-      'text-white font-italiana z-10 top-0 bottom-0 left-0 right-0 text-center content-center text-5xl',
+      'text-white font-italiana z-10 top-0 bottom-0 left-0 right-0 text-center content-center text-5xl font-bold',
       className,
     ),
   )
