08119e2e0294d60131a8120812bc6c16d625be7e
gibsfinancedev <162373480+gibsfinancedev@users.noreply.github.com>
2025-05-12 09:22:31 -0500

    Adjsuts welcome layout for smaller screens

--- src/lib/components/Welcome.svelte
+++ src/lib/components/Welcome.svelte
@@ -1,6 +1,7 @@
 <script>
   import BlurryImage from './BlurryImage.svelte'
   import { onMount } from 'svelte'
+  import { innerWidth } from 'svelte/reactivity/window'
   const payMe = 'images/pay-me.png'
   let toggle = $state(false)
   onMount(() => {
@@ -9,6 +10,7 @@
     }, 1000)
     return () => clearTimeout(id)
   })
+  const isSmall = $derived(innerWidth.current && innerWidth.current < 768)
 </script>
 
 <BlurryImage height="600px" image_url="url({payMe})">
@@ -19,13 +21,13 @@
       class:translate-y-8={!toggle}>
       Gibs
     </h1>
-    <div class="flex flex-row gap-2 italic font-light">
+    <div class="flex gap-2 font-light text-center" class:flex-col={isSmall} class:flex-row={!isSmall}>
       <span
         class="transition-all duration-300"
         class:translate-y-full={!toggle}
         class:opacity-0={!toggle}>Convenience.</span>
       <span
-        class="transition-all duration-300 delay-700"
+        class="transition-all italic duration-300 delay-700"
         class:translate-y-full={!toggle}
         class:opacity-0={!toggle}>For Pulsechain.</span>
     </div>
